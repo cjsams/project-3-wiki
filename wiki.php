a64e1fe558018abc21190b6793af75ef22a11003
cjsams <csams@mail.bradley.edu>
2015-11-12 03:08:55 -0600

    commit
    html

--- wiki.php
+++ wiki.php
@@ -1,16 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Wiki</title>
+    </head>
+    <body>
+      <?php
 
-if (isset($_GET['content'])) {
-    $content = $_GET['content'];
-    file_put_contents('wiki.txt', $content);
-}
+      if (isset($_GET['content'])) {
+        $content = $_GET['content'];
+        file_put_contents('wiki.txt', $content);
+    }
 
-if (file_exists('wiki.txt')) {
-    $content = file_get_contents('wiki.txt');
-} else {
-    $content = '(no content)';
-}
+    if (file_exists('wiki.txt')) {
+        $content = file_get_contents('wiki.txt');
+    } else {
+        $content = '(no content)';
+    }
 
-$safe_content = htmlentities($content);
-echo $safe_content;
-?>
+    $safe_content = htmlentities($content);
+    echo $safe_content;
+    ?>
+  <form action="wiki.php">
+    <textarea name="content" rows="8" cols="80"></textarea>
+    <input type="submit" value="Save">
+  </form>
+  </body>
+</html>
