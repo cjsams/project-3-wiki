7c5e29af8023c4540b1325910b8c4903b256eef1
cjsams <csams@mail.bradley.edu>
2015-11-12 07:08:16 -0600

    ulj

--- wiki.php
+++ wiki.php
@@ -26,7 +26,7 @@
     echo $safe_content;
     ?>
 
-  <form action="wiki.php" class="hidden">
+  <form class="hidden">
 
     <textarea name="content" rows="8" cols="80"></textarea>
     <input type="submit" value="Save">
